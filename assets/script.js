13ecf9666c6d988ef2d0e5ffc5bbb7ce04eceb20
Will D <will.dean0103@gmail.com>
2022-08-31 13:23:12 -0400

    messed around with JavaScript

--- assets/script.js
+++ assets/script.js
@@ -1,10 +1,13 @@
+
 var apiKey = 'a7e3e75b78414b8ab5d3b80de054658a';
 var submitButton = document.querySelector('#searchGame');
+var screenShotEl = document.querySelector('#screenshot');
+
 
 function getGameInfo(event) {
     var gameName = document.querySelector('#gameName');
     var newURL = `https://api.rawg.io/api/games?key=${apiKey}&search=${gameName.value}`;
-
+    
     event.preventDefault();
     fetch(newURL)
     .then(function (response) {
@@ -12,7 +15,15 @@ function getGameInfo(event) {
     })
     .then(function (data) {
         console.log(data.results);
+        displayScreenshot(data);
     })
-}
+};
+
+function displayScreenshot(data) {
+    var i = 0;
+    var imgEl = document.createElement('img');
+    imgEl.src = data[0].short_screenshots[i].image;
+    screenShotEl.appendChild(imgEl);
+};
 
 submitButton.addEventListener('click', getGameInfo)
