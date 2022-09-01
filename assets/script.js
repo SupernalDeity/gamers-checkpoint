a0025a26f68f4b99ea2027312db5bfa238c7cb28
Will D <will.dean0103@gmail.com>
2022-09-01 13:41:25 -0400

    added initial load page

--- assets/script.js
+++ assets/script.js
@@ -126,3 +126,117 @@ function getAllGameInfo() {
     submitButton.addEventListener('click', getAllGameInfo);
     lastBtn.addEventListener('click', prevImg);
     nextBtn.addEventListener('click', nextImg);
+
+    function initialDisplayGame() {
+        initialGameInfo()
+        initialGameGiphy()
+    };
+    
+    function initialGameInfo() {
+        var gameName = "Cyberpunk 2077";
+        var newURL = `https://api.rawg.io/api/games?key=${apiKey}&search=${gameName}`;
+    
+        fetch(newURL)
+            .then(function (response) {
+                return response.json();
+            })
+            .then(function (data) {
+                console.log(data.results);
+                initialDisplayScreenshot(data);
+            })
+    };
+    
+    function initialDisplayScreenshot(data) {
+        gameInfoBody.innerHTML = null;
+        imgEl.src = data.results[0].short_screenshots[i].image;
+    
+        var nameEl = document.createElement('h1');
+        var esrbEl = document.createElement('p');
+        var metacriticEl = document.createElement('p');
+        var genreshEl = document.createElement('h4');
+        var ratingScoreEl = document.createElement('p');
+        var ratingshEl = document.createElement('h4');
+        var esrbhEl = document.createElement('h4');
+    
+        nameEl.className = "text-center"
+    
+        nameEl.textContent = data.results[0].name;
+        esrbEl.textContent = "ESRB Rating: " + data.results[0].esrb_rating.name;
+        metacriticEl.textContent = "Metacritic: " + data.results[0].metacritic;
+        ratingScoreEl.textContent = "Rating: " + data.results[0].rating;
+        genreshEl.textContent = 'Genres: ';
+        ratingshEl.textContent = 'Ratings: ';
+        esrbhEl.textContent = 'ESRB: ';
+    
+        gameInfoBody.appendChild(nameEl);
+        gameInfoBody.appendChild(esrbhEl);
+        gameInfoBody.appendChild(esrbEl);
+        gameInfoBody.appendChild(genreshEl);
+        for (var y = 0; y < data.results[0].genres.length; y++) {
+            var genresEl = document.createElement('p');
+            genresEl.textContent = '' + data.results[0].genres[y].name;
+            gameInfoBody.appendChild(genresEl);
+        };
+        gameInfoBody.appendChild(ratingshEl);
+        gameInfoBody.appendChild(metacriticEl);
+        gameInfoBody.appendChild(ratingScoreEl);
+        for (var x = 0; x < data.results[0].ratings.length; x++) {
+            var ratingsEl = document.createElement('p');
+            ratingsEl.textContent = 'Rating: ' + data.results[0].ratings[x].title + ' -- Count: ' + data.results[0].ratings[x].count + ' -- ' + data.results[0].ratings[x].percent + '%';
+            gameInfoBody.appendChild(ratingsEl);
+        };
+    
+    };
+    
+    function prevImg() {
+        if (i == 0) {
+            i == 6;
+        }
+        else {
+            i--;
+        }
+        initialGameInfo();
+    };
+    
+    function nextImg() {
+        if (i == 6) {
+            i == 0;
+        }
+        else {
+            i++;
+        }
+        initialGameInfo();
+    };
+    
+    function initialDisplayGiphy(data) {
+        giphySection.innerHTML = null;
+        for (var g = 0; g < 5; g++){
+            var divGEl = document.createElement('div');
+            var imgGEl = document.createElement('img');
+    
+            divGEl.className = 'card col-12 col-md-2 m-4 border-0';
+            divGEl.style.width = '18rem';
+    
+            imgGEl.className = 'card-img-top';
+            imgGEl.src = data.data[g].images.original.url;
+    
+            giphySection.appendChild(divGEl);
+            divGEl.appendChild(imgGEl);
+        };
+    };
+    
+    function initialGameGiphy() {
+        var giphySearch = "Cyberpunk 2077";
+        var url = `https://api.giphy.com/v1/gifs/search?q=${giphySearch}&api_key=${giphyApiKey}&rating=pg-13`;
+    
+        fetch(url)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data.data);
+            initialDisplayGiphy(data);
+        })
+    };
+    
+    initialDisplayGame();
