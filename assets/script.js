aa298b8f449257a461435353c1b6ff1aa92e8647
Anthony <perezant121@gmail.com>
2022-09-01 23:19:57 -0400

    rerouted functions to clean up repeating code

--- assets/script.js
+++ assets/script.js
@@ -12,11 +12,14 @@ var buttonsEl = document.querySelector('#buttons');
 var searchedGamesEl = document.querySelector('#searchedGames');
 
 function getGameInfo(event) {
-    var gameName = document.querySelector('#gameName');
-    var newURL = `https://api.rawg.io/api/games?key=${apiKey}&search=${gameName.value}`;
-
     event.preventDefault();
-    fetch(newURL)
+    var gameName = document.querySelector('#gameName');
+    var gameUrl = `https://api.rawg.io/api/games?key=${apiKey}&search=${gameName.value}`;
+    searchGameInfo(gameUrl);
+};
+
+function searchGameInfo (gameUrl){
+    fetch(gameUrl)
         .then(function (response) {
             return response.json();
         })
@@ -118,10 +121,13 @@ function displayGiphy(data) {
 };
 
 function getGameGiphy(event) {
+    event.preventDefault();
     var giphySearch = document.querySelector('#gameName');
     var url = `https://api.giphy.com/v1/gifs/search?q=${giphySearch.value}&api_key=${giphyApiKey}&rating=pg-13`;
+    getGiphyInfo(url);
+};
 
-    event.preventDefault();
+function getGiphyInfo (url) {
     fetch(url)
         .then(function (response) {
             return response.json();
@@ -142,9 +148,6 @@ function saveToLocal() {
     getGameGiphy(event);
 };
 
-
-
-
 function initialDisplayGame() {
     initialGameInfo()
     initialGameGiphy()
@@ -152,114 +155,25 @@ function initialDisplayGame() {
 
 function initialGameInfo() {
     var gameName = "";
-    var newURL = `https://api.rawg.io/api/games?key=${apiKey}&search=${gameName}`;
-    
-    fetch(newURL)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data.results);
-        initialDisplayScreenshot(data);
-    })
-};
-
-function initialDisplayScreenshot(data) {
-    gameInfoBody.innerHTML = null;
-    imgEl.src = data.results[0].short_screenshots[0].image;
-    
-    var nameEl = document.createElement('h1');
-    var esrbEl = document.createElement('p');
-    var metacriticEl = document.createElement('p');
-    var genreshEl = document.createElement('h4');
-    var ratingScoreEl = document.createElement('p');
-    var ratingshEl = document.createElement('h4');
-    var esrbhEl = document.createElement('h4');
-    
-    var displayAllSearches = JSON.parse(localStorage.getItem('localGames')) || [];
-    for (var v = 0; v < displayAllSearches.length; v++) {
-        var btn = document.createElement('button');
-        btn.className = 'btn btn-primary mx-2';
-        btn.textContent = displayAllSearches[v];
-        searchedGamesEl.appendChild(btn);
-    };
-    
-    nameEl.className = "text-center"
-    
-    nameEl.textContent = data.results[0].name;
-    esrbEl.textContent = "ESRB Rating: " + data.results[0].esrb_rating.name;
-    metacriticEl.textContent = "Metacritic: " + data.results[0].metacritic;
-    ratingScoreEl.textContent = "Rating: " + data.results[0].rating;
-    genreshEl.textContent = 'Genres: ';
-    ratingshEl.textContent = 'Ratings: ';
-    esrbhEl.textContent = 'ESRB: ';
-    
-    gameInfoBody.appendChild(nameEl);
-    gameInfoBody.appendChild(esrbhEl);
-    gameInfoBody.appendChild(esrbEl);
-    gameInfoBody.appendChild(genreshEl);
-    for (var y = 0; y < data.results[0].genres.length; y++) {
-        var genresEl = document.createElement('p');
-        genresEl.textContent = '' + data.results[0].genres[y].name;
-        gameInfoBody.appendChild(genresEl);
-    };
-    gameInfoBody.appendChild(ratingshEl);
-    gameInfoBody.appendChild(metacriticEl);
-    gameInfoBody.appendChild(ratingScoreEl);
-    for (var x = 0; x < data.results[0].ratings.length; x++) {
-        var ratingsEl = document.createElement('p');
-        ratingsEl.textContent = 'Rating: ' + data.results[0].ratings[x].title + ' -- Count: ' + data.results[0].ratings[x].count + ' -- ' + data.results[0].ratings[x].percent + '%';
-        gameInfoBody.appendChild(ratingsEl);
-    };
-    
-};
-
-function initialDisplayGiphy(data) {
-    giphySection.innerHTML = null;
-    for (var g = 0; g < 5; g++) {
-        var divGEl = document.createElement('div');
-        var imgGEl = document.createElement('img');
-        
-        divGEl.className = 'card col-12 col-md-2 m-4 border-0';
-        divGEl.style.width = '18rem';
-        
-        imgGEl.className = 'card-img-top';
-        imgGEl.src = data.data[g].images.original.url;
-        
-        giphySection.appendChild(divGEl);
-        divGEl.appendChild(imgGEl);
-    };
+    var gameUrl = `https://api.rawg.io/api/games?key=${apiKey}&search=${gameName}`;
+    searchGameInfo(gameUrl);
 };
 
 function initialGameGiphy() {
     var giphySearch = "GTA 5";
     var url = `https://api.giphy.com/v1/gifs/search?q=${giphySearch}&api_key=${giphyApiKey}&rating=pg-13`;
-    
-    fetch(url)
-    .then(function (response) {
-        return response.json();
-    })
-    .then(function (data) {
-        console.log(data.data);
-        initialDisplayGiphy(data);
-    })
+    getGiphyInfo(url);
 };
-
-initialDisplayGame();
 
 function oldSearches(event) {
     event.preventDefault();
     if (event.target.matches('button')) {
         var oldGameSearch = event.target.textContent;
-        console.log(oldGameSearch);
-        var btnUrl = `https://api.rawg.io/api/games?key=${apiKey}&search=${oldGameSearch}`;
-        fetch(btnUrl)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            displayScreenshot(data);
-        })
+        var gameUrl = `https://api.rawg.io/api/games?key=${apiKey}&search=${oldGameSearch}`;
+        searchGameInfo(gameUrl);
+        var giphySearch = event.target.textContent;
+        var url = `https://api.giphy.com/v1/gifs/search?q=${giphySearch}&api_key=${giphyApiKey}&rating=pg-13`;
+        getGiphyInfo(url);
     };
 };
 
@@ -267,3 +181,5 @@ submitButton.addEventListener('click', saveToLocal);
 lastBtn.addEventListener('click', prevImg);
 nextBtn.addEventListener('click', nextImg);
 searchedGamesEl.addEventListener('click', oldSearches);
+
+initialDisplayGame();
